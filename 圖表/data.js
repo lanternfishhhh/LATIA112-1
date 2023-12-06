710be45e3486fd4ea0d6825407308c18ac7379ec
lanternfishhhh <144101414+lanternfishhhh@users.noreply.github.com>
2023-12-06 16:49:17 +0800

    Add files via upload

---
+++ 圖表/data.js
@@ -0,0 +1,95 @@
+
+let national=[
+    {
+        "name":"北部",
+        "count":20
+    },
+    {
+        "name":"中部",
+        "count":10
+    },
+    {
+        "name":"南部",
+        "count":15
+    },
+    {
+        "name":"東部",
+        "count":3
+    },
+    {
+        "name":"金門",
+        "count":1
+    },
+];
+let private=[
+    {
+        "name":"北部",
+        "count":53
+    },
+    {
+        "name":"中部",
+        "count":22
+    },
+    {
+        "name":"南部",
+        "count":30
+    },
+    {
+        "name":"東部",
+        "count":3
+    },
+    {
+        "name":"金門",
+        "count":0
+    },
+];
+let north_college=[
+    {
+        "name":"國立大學",
+        "count":20
+    },
+    {
+        "name": "私立大學",
+        "count": 53
+    }
+];
+let middle_college=[
+    {
+        "name":"國立大學",
+        "count":10
+    },
+    {
+        "name": "私立大學",
+        "count": 22
+    }
+];
+let south_college=[
+    {
+        "name":"國立大學",
+        "count":15
+    },
+    {
+        "name": "私立大學",
+        "count": 30
+    }
+];
+let east_college=[
+    {
+        "name":"國立大學",
+        "count":3
+    },
+    {
+        "name": "私立大學",
+        "count": 3
+    }
+];
+let kinmen_college=[
+    {
+        "name":"國立大學",
+        "count":1
+    },
+    {
+        "name": "私立大學",
+        "count": 0
+    }
+]
